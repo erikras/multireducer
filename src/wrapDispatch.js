3521fa15532a5c9b1fdcf5ddea961f286ec689d7
Anton Kostenko <kostenko.antonn@gmail.com>
2016-12-02 17:25:24 +0300

    Add support for redux-thunk with extra argument

--- src/wrapDispatch.js
+++ src/wrapDispatch.js
@@ -4,8 +4,8 @@ export default function wrapDispatch(dispatch, reducerKey) {
   const wrappedDispatch = (action) => {
     let wrappedAction;
     if (typeof action === 'function') {
-      wrappedAction = (globalDispatch, getState) =>
-        action(wrappedDispatch, getState, globalDispatch, reducerKey);
+      wrappedAction = (globalDispatch, getState, extraArgument) =>
+        action(wrappedDispatch, getState, globalDispatch, reducerKey, extraArgument);
     } else if (typeof action === 'object') {
       wrappedAction = wrapAction(action, reducerKey);
     }
